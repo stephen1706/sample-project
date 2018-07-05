76e505802f2e20ffa22f930bba69b92f4750bb28
Stephen Adipradhana <stephen_adipradhana@astro.com.my>
2018-07-05 15:50:39 +0800

    add interface to repository class

---
+++ Readme.md
@@ -0,0 +1,7 @@
+- what was your assumptions and design considerations when doing the exercise
+    No specific assumptions were made actually.
+    The design of the code is using MVVM.
+    Unit test can be done by mocking the repository class and we can unit test the logic of the view model class
+
+- what did you find most difficult
+- any other comments you would like to tell us
---
+++ app/src/main/java/com/stephen/shopback/repositories/UserRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.stephen.shopback.repositories
+
+import com.stephen.shopback.api.ApiRoutes
+import com.stephen.shopback.api.ApiService
+import com.stephen.shopback.api.UserDataModel
+import com.stephen.shopback.api.UserListDataModel
+import com.stephen.shopback.utils.PageLinks
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.json.JSONObject
+
+
+class UserRepositoryImpl (private val apiService: ApiService) : UserRepository{
+    override fun getUserList(url: String?): Single<UserListDataModel> {
+        return apiService.getUsers(url ?: ApiRoutes.USER_LIST_ROUTES + "?per_page=20")
+                .map {
+                    if(it.code() < 200 || it.code() >= 300){
+                        var message: String = "Error " + it.code()
+                        try {
+                            val json = JSONObject(it.errorBody()?.string())
+                            message = json.getString("message")
+                        } catch (e: Exception){
+                        }
+                        throw Throwable(message)
+                    }
+                    val nextUrl = PageLinks(it.headers()["Link"]?: "").next
+                    UserListDataModel(it.body() ?: ArrayList(), nextUrl)
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    override fun getUserDetail(id: String): Single<UserDataModel> {
+        return apiService.getUserDetail(id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+}
